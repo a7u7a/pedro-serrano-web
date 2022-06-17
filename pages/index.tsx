dc15c42b55d709ea8eee2031ceb4606231fc55c2
a7u7a <esteban@esrs.co>
2022-06-17 14:01:32 -0400

    add test boc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
+import TestBox from "../components/testBox";
 const Home: NextPage = () => {
   return (
     <div>
@@ -12,24 +11,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main className="p-4 min-h-screen flex flex-col justify-center items-center">
         <h1 className="text-3xl font-bold underline decoration-wavy">
           Hello World Pedro Serrano Web!
         </h1>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+        <TestBox />
+      </main>
     </div>
   );
 };
