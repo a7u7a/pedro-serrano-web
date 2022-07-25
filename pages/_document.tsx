c7408dfb377cf083cb682c8f1685b19d8c837c2e
a7u7a <esteban@esrs.co>
2022-07-25 16:29:28 -0400

    cms setup

---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Netlify Widget */}
+          <script
+            async
+            src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          />
+          {/* Fonts */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
