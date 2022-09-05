74d843f33dfbc5f6967b107dd47ac99c2b7d8a61
a7u7a <esteban@esrs.co>
2022-09-05 16:36:58 -0300

    fix credit

--- components/credits.tsx
+++ components/credits.tsx
@@ -1,10 +1,17 @@
 const Credits = () => {
   return (
-    <div className="text-white text-right pr-4 sm:pr-6 pb-4 font-regular text-xl md:text-2xl">
-      <p>
-        Created by&nbsp;
-        <a className="hover:underline decoration-2 underline-offset-4 text-indigo-600" href="http://esrs.co/">esrs.co</a>
-      </p>
+    <div className="absolute inset-x-0 bottom-0">
+      <div className="text-white text-right pr-4 sm:pr-6 pb-4 font-regular text-xl md:text-2xl">
+        <p>
+          Created by&nbsp;
+          <a
+            className="hover:underline decoration-2 underline-offset-4 text-indigo-600"
+            href="http://esrs.co/"
+          >
+            esrs.co
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
