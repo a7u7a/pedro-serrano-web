a9337d9c79a0d437196ea5da29d6a77515807ad8
a7u7a <esteban@esrs.co>
2022-09-05 22:32:18 -0400

    testing credits fix

--- components/main-scene.tsx
+++ components/main-scene.tsx
@@ -68,13 +68,9 @@ export default function MainScene({
       document.documentElement.clientHeight || 0,
       window.innerHeight || 0
     );
-    // const pages = (mainBounds.height / vh) - (isMobile ? 0.8 : 0);
-    const pages = mainBounds.height / vh;
+    const pages = (mainBounds.height / vh) - (isMobile ? 0.8 : 0);
+    // const pages = mainBounds.height / vh;
     setTotalPages(pages);
-    // console.log("vh", vh);
-    // console.log("mainBounds", mainBounds);
-    // console.log("window.innerHeight", window.innerHeight);
-    // console.log("pages", pages);
   }, [mainBounds]);
 
   const scene = (
@@ -189,7 +185,7 @@ export default function MainScene({
                   <MainText />
                   <Gallery posts={experimentsProj} category={"Experiments"} />
                   <MyFooter />
-                  {/* <Credits /> */}
+                  <Credits />
                 </div>
               </Scroll>
             </ScrollControls>
